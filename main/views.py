fc5c9dcea26aefc88bbe9162efad1b57a2cf5635
katsukikuroda <katsukikuroda@gmail.com>
2024-12-04 18:02:57 +0900

    1stcmt

--- main/views.py
+++ main/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import redirect, render
+from .forms import SignUpForm
 
 def index(request):
     return render(request, 'main/index.html')
 
 def signup(request):
-    return render(request, 'main/signup.html')
+    # ↓26で変更
+    if request.method == "GET":
+        form = SignUpForm()
+
+    elif request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                auth.login(request, user)
+
+            return redirect("index")
+
+    context = {"form": form}
+    return render(request, "main/signup.html", context)
 
 def login(request):
     return render(request, 'main/login.html')
